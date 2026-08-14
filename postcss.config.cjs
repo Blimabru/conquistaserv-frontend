ae8bc7a13d318197aa3401853386b18b34e18a52
Bruno Lima <bru.no@outlook.com.br>
2026-08-14 17:49:07 -0300

    fix(build): downgrade do tailwindcss para v3 compatível com quasar vite

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,6 @@
 /* eslint-disable */
 // https://github.com/michael-ciniawsky/postcss-load-config
-const tailwindcss = require('@tailwindcss/postcss')
+const tailwindcss = require('tailwindcss')
 
 module.exports = {
   plugins: [
